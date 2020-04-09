b40c983acd3ddc59f31c3c2034c5eb0c06d73bbe
aramonh <aramonh@unal.edu.co>
2020-04-08 21:41:49 -0500

    driver auth, TOKENS TODO

---
+++ rideSupport_profileDriver_ms/app/Http/Controllers/driverController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Driver;
+class driverController extends Controller
+{
+
+    public function login(Request $request){
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        $drivers= Driver::where('email',$email)->first();
+
+            if($drivers==""){
+                return response("Username/Email does not exist, please register.");
+            }else{
+                if($drivers["password"]!=$password) {
+                    return response("The password is wrong, try again with another one.");
+                }else{
+                    //TODO Devolver TOKENs
+                    return response()->json($drivers);
+                }
+            }
+            return 0;
+    }
+
+    public function create(Request $request){
+        $drivers = new Driver();
+
+        $drivers["name"] = $request->input('name');
+        $drivers["email"] = $request->input('email');
+        $drivers["password"] = $request->input('password');
+    
+        $drivers->save();
+        return response()->json($drivers);
+    }
+
+    public function getAll(){
+        $drivers= Driver::all();
+        return response()->json($drivers);
+    }
+
+  
+    public function getById($id){
+        $drivers= Driver::find($id);
+        return response()->json($drivers);
+    }
+
+    public function updateById($id, Request $request){
+        $drivers= Driver::find($id);
+
+        $drivers["name"] = $request->input('name');
+        $drivers["email"] = $request->input('email');
+        $drivers["password"] = $request->input('password');
+    
+        $drivers->save();
+        return response()->json($drivers);
+    }
+
+
+
+    public function deleteById($id, Request $request){
+        $drivers= Driver::find($id);
+        $drivers->delete();
+        return response()->json($drivers);
+    }
+    
+}
